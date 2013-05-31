2e0d0dc77239b4dfa83bd2cebbfdeebf393dfed0
Andreas Brekken <a@abrkn.com>
2013-05-31 11:27:12 +0200

    Ignore files that are not number prefixed.

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 var fs = require('fs')
-, util = require('util')
 , pgm = module.exports = {}
 
 pgm.number = function(filename) {
     var match = /^(\d+).*\.sql$/i.exec(filename)
-    if (!match) throw new Error(util.format('%s must start with a number and end in .sql', filename))
+    if (!match) return null
     return +match[1]
 }
 
 pgm.find = function(dir, from, to) {
     return fs.readdirSync(dir)
     .filter(function(fn) {
-        return /\.sql/i.test(fn)
+        return pgm.number(fn) !== null
     })
     .map(function(fn) {
         return {
             number: pgm.number(fn),
-            filename: fn,
+            filename: fn
         }
     })
     .filter(function(f) {
